031a89cf440b20fba593aeea5cedb3536069f671
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2025-12-17 14:49:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,6 @@ st.set_page_config(
 )
 st.title("💬 Markelytics - Data Lake")
 st.caption("Chat with your OneDrive CASE_STUDIES documents")
-st.markdown(
-    """
-    <style>
-    .stAppHeader{
-        display: none !important;
-    }
-    .._container_gzau3_1{
-        display: none !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 
 # ---------------------------------
 # Vector DB status
@@ -117,6 +103,7 @@ if prompt:
                     "role": "assistant",
                     "content": error_msg
                 })
+
 
 
 
